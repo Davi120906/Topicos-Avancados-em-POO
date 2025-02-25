87d3a4fa97b4ab41a1fedaccf23e5bc564c09cb9
Davi120906 <davichalfum@gmail.com>
2025-02-25 20:38:12 -0300

    Upando ativ1

---
+++ ativ1/Expressoes/Program.cs
@@ -0,0 +1,14 @@
+﻿using CAS;
+using System.Numerics;
+
+Expressao a = 10;
+Expressao b = "b";
+
+Expressao soma = a * 10;
+Expressao c = 50;
+
+Complexos teste = new Complexos(5,5);
+Expressao som = teste.Derivar((b as Simbolo));
+
+Console.WriteLine(som.Simplificar());
+
